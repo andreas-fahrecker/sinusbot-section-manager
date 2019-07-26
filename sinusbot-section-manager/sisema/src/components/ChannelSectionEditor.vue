aef8760c83e43ee449349b0dcba325735cccc560
Andreas Fahrecker <andreasfahrecker@gmail.com>
2019-07-27 01:44:14 +0200

    - Modified ChannelSectionEditor to contain view/edit mode and save, delete and cancel button
    Signed-off-by: Andreas Fahrecker <andreasfahrecker@gmail.com>

--- sinusbot-section-manager/sisema/src/components/ChannelSectionEditor.vue
+++ sinusbot-section-manager/sisema/src/components/ChannelSectionEditor.vue
@@ -14,8 +14,21 @@
                                       :key="index" v-model="sectionChannel.permissions[index]"
                                       :readonly="viewMode"/>
         </b-form>
-        <b-row slot="footer">
-            <b-col>Edit Button n Stuff</b-col>
+        <b-row v-if="viewMode" slot="footer">
+            <b-col>
+                <b-button block variant="primary" @click="editOnClick">Edit</b-button>
+            </b-col>
+        </b-row>
+        <b-row v-else slot="footer">
+            <b-col>
+                <b-button block variant="primary" @click="saveOnClick">Save</b-button>
+            </b-col>
+            <b-col>
+                <b-button block variant="danger" @click="deleteOnClick">Delete</b-button>
+            </b-col>
+            <b-col>
+                <b-button block variant="warning" @click="cancelOnClick">Cancel</b-button>
+            </b-col>
         </b-row>
     </b-card>
 </template>
@@ -52,7 +65,20 @@
                 viewMode: true
             };
         },
-        methods: {}
+        methods: {
+            editOnClick() {
+                this.viewMode = false;
+            },
+            saveOnClick() {
+
+            },
+            deleteOnClick() {
+
+            },
+            cancelOnClick() {
+                this.viewMode = true;
+            }
+        }
     }
 </script>
 
