363c723ff278f364fe1f68628ab89529166d8f8d
Andreas Fahrecker <andreasfahrecker@gmail.com>
2019-07-29 01:20:40 +0200

    - Removed now unnecessary comments
    Signed-off-by: Andreas Fahrecker <andreasfahrecker@gmail.com>

--- sinusbot-section-manager/sisema/src/components/section-inputs/SectionAudioQualityInput.vue
+++ sinusbot-section-manager/sisema/src/components/section-inputs/SectionAudioQualityInput.vue
@@ -2,10 +2,8 @@
     <b-form-row>
         <b-col>
             <b-form-group label="Section Codec" label-for="sectionCodecInput">
-                <!--suppress XmlDuplicatedId -->
                 <b-form-input v-if="readonly" id="sectionCodecInput"
                               v-model="codecs.filter(value => value.value === codec)[0].text" type="text" readonly/>
-                <!--suppress XmlDuplicatedId -->
                 <b-form-select v-else id="sectionCodecInput" :value="codec" @input="updateCodec($event)"
                                :options="codecs"/>
             </b-form-group>
